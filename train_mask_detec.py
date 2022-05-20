126e2b480fe12738f6f13facb69a0cde5c5bfb11
Amstf <ali_mustapha_@outlook.com>
2022-05-20 16:25:04 +0200

    fixing code

--- train_mask_detec.py
+++ train_mask_detec.py
@@ -26,7 +26,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\Users\Ali Mustapha\Desktop\FaceMask\dataset"
+DIRECTORY = r"FaceMask\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
