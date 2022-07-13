54c1c37ee1ec52bca34ccb4c339d7ca34cd9fcaf
Amstf <ali_mustapha_@outlook.com>
2022-07-13 16:18:57 +0200

    updates in the face detectors

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -2,9 +2,10 @@
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
+import imutils
+
 from imutils.video import VideoStream
 import numpy as np
-import imutils
 import time
 import cv2
 import os
